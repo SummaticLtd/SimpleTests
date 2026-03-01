5ec5665cefc210a3b296bb262559eb8c2763afe5
Charles Roddie <charles.roddie@summatic.co.uk>
2026-03-01 03:17:12 +0000

    library and test project with a single test

---
+++ Example/Program.fs
@@ -0,0 +1,29 @@
+﻿module Program
+
+open SimpleTests
+open Microsoft.Testing.Platform.Builder
+open Microsoft.Testing.Platform.Capabilities.TestFramework
+open Microsoft.Testing.Platform.Extensions.TestFramework
+
+[<EntryPoint>]
+let main (args: string array) =
+    task {
+        let! builder = TestApplication.CreateBuilderAsync(args)
+
+        builder.RegisterTestFramework(
+            (fun _ -> SimpleCapabilities() :> ITestFrameworkCapabilities),
+            (fun _ _ ->
+                SimpleFramework(
+                    "two-plus-two",
+                    "2 + 2 should equal 4",
+                    (fun () -> 2 + 2 = 4),
+                    "Expected 2 + 2 to equal 4"
+                )
+                :> ITestFramework)
+        )
+        |> ignore
+
+        let! app = builder.BuildAsync()
+        return! app.RunAsync()
+    }
+    |> fun t -> t.GetAwaiter().GetResult()
---
+++ SimpleTests/Library.fs
@@ -0,0 +1,73 @@
+﻿namespace SimpleTests
+
+open System.Threading.Tasks
+open Microsoft.Testing.Platform.Capabilities.TestFramework
+open Microsoft.Testing.Platform.Extensions
+open Microsoft.Testing.Platform.Extensions.Messages
+open Microsoft.Testing.Platform.Extensions.TestFramework
+open Microsoft.Testing.Platform.Requests
+
+// -- Capabilities (none needed for this demo) --
+type SimpleCapabilities() =
+    interface ITestFrameworkCapabilities with
+        member _.Capabilities = [||]
+
+// -- Test framework that runs a single test --
+type SimpleFramework(testUid: string, testName: string, runTest: unit -> bool, failMessage: string) as self =
+    interface IExtension with
+        member _.Uid = "SimpleFramework"
+        member _.Version = "1.0.0"
+        member _.DisplayName = "Simple Framework"
+        member _.Description = "Minimal direct Microsoft.Testing.Platform demo"
+        member _.IsEnabledAsync() = Task.FromResult(true)
+
+    interface IDataProducer with
+        member _.DataTypesProduced = [| typeof<TestNodeUpdateMessage> |]
+
+    interface ITestFramework with
+        member _.CreateTestSessionAsync(_context) =
+            Task.FromResult(CreateTestSessionResult())
+
+        member _.CloseTestSessionAsync(_context) =
+            Task.FromResult(CloseTestSessionResult())
+
+        member _.ExecuteRequestAsync(context) =
+            task {
+                match context.Request with
+                | :? DiscoverTestExecutionRequest as request ->
+                    let testNode =
+                        TestNode(
+                            Uid = TestNodeUid(testUid),
+                            DisplayName = testName,
+                            Properties = PropertyBag(DiscoveredTestNodeStateProperty())
+                        )
+
+                    let message =
+                        TestNodeUpdateMessage(request.Session.SessionUid, testNode)
+
+                    do! context.MessageBus.PublishAsync(self, message)
+
+                | :? RunTestExecutionRequest as request ->
+                    let passed = runTest ()
+
+                    let stateProperty: IProperty =
+                        if passed then
+                            PassedTestNodeStateProperty(testName) :> IProperty
+                        else
+                            FailedTestNodeStateProperty(failMessage) :> IProperty
+
+                    let testNode =
+                        TestNode(
+                            Uid = TestNodeUid(testUid),
+                            DisplayName = testName,
+                            Properties = PropertyBag(stateProperty)
+                        )
+
+                    let message =
+                        TestNodeUpdateMessage(request.Session.SessionUid, testNode)
+
+                    do! context.MessageBus.PublishAsync(self, message)
+                | _ -> ()
+
+                context.Complete()
+            }
