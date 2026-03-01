3994ed2fae5e4dc08a1f9c98130f026bc361195c
Charles Roddie <charles.roddie@summatic.co.uk>
2026-03-01 03:57:45 +0000

    test collectio types

--- SimpleTests/Types.fs
+++ SimpleTests/Types.fs
@@ -3,9 +3,19 @@
 open System.Threading.Tasks
 open System.Collections.Generic
 
+[<RequireQualifiedAccess>]
 type Test =
     | Sync of uniqueName: string * run: (unit -> Result<unit, string>)
     | Async of uniqueName: string * run: (unit -> Task<Result<unit, string>>)
+    | CasesSync of uniqueName: string * cases: IReadOnlyCollection<string * (unit -> Result<unit, string>)>
+    | CasesAsync of uniqueName: string * cases: IReadOnlyCollection<string * (unit -> Task<Result<unit, string>>)>
 
-type TestList(tests: IReadOnlyCollection<Test>) =
+/// A collection of Tests, in VS Test Explorer corresponding to a "Class".
+type TestList(name: string, tests: IReadOnlyCollection<Test>) =
+    member _.Name = name
     member _.Tests = tests
+
+/// A collection of test lists, in VS Test Explorer corresponding to a "Namespace".
+type TestFolder(namespaceName: string, testLists: IReadOnlyCollection<TestList>) =
+    member _.NamespaceName = namespaceName
+    member _.TestLists = testLists
